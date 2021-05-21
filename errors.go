f60a2b0a07a7fead9d2f78245cacdeb58cceed20
8i8 <iain.coding@gmail.com>
2021-05-21 16:07:51 +0200

    init

---
+++ errors.go
@@ -0,0 +1,10 @@
+package meta
+
+import "errors"
+
+// Application error messages.
+var Err03Activation = errors.New("error activation")
+var Err05Request = errors.New("error request")
+var Err07User = errors.New("error user")
+var Err08Resource = errors.New("error resource")
+var Err09Record = errors.New("error record")
