4bc938f019ffbf4d85ec81132d0e5122e0ffa724
8i8 <iain.coding@gmail.com>
2021-05-23 12:32:02 +0200

    comments

--- errors.go
+++ errors.go
@@ -3,8 +3,20 @@ package meta
 import "errors"
 
 // Application error messages.
+
+// Err03Activation not ready to be run.
 var Err03Activation = errors.New("error activation")
+
+// Err05Request some thing made a poorly formated request.
 var Err05Request = errors.New("error request")
+
+// Err07User a user error has occurred.
 var Err07User = errors.New("error user")
+
+// Err08Resource resource error, a resource has changed or is not
+// present.
 var Err08Resource = errors.New("error resource")
+
+// Err09Record the data received was not expected or is not the correct
+// data.
 var Err09Record = errors.New("error record")
